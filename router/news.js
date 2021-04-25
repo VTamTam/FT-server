2cc21b86fbbe537d12efb9417f5a5d582c2ca9f7
Erwin Zarghani <erwinzarghani@Erwins-MacBook-Pro.local>
2021-04-25 14:24:45 +0100

    ok

--- router/news.js
+++ router/news.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+//const router = require('express').Router();
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -7,34 +7,28 @@ const transporter = nodemailer.createTransport({
     pass: process.env.PASS,
   },
 });
-
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
 
 router.route("/").get((req, res) => {
+  const api_url =process.env.APP_URL;
   console.log(req.query);
   const query = {};
-  if (req.query.category) {
-    query.category = req.query.category;
+  if (req.query.name) {
+    query.name = req.query.name;
   }
   if (req.query.searchValue) {
     let regex = new RegExp(req.query.searchValue, "i");
-    query.$or = [{ title: regex }, { question: regex }, { category: regex }];
+    query.$or = [{ name: regex }, { description: regex }];
   }
-  Question.find(query)
-    .sort({
-      createdAt: -1,
-    })
-    .populate("answers")
-    .then((questions) => {
-      if (request.query.noAnswer === "true") {
-        const filteredQuestions = questions.filter(
-          (question) => question.answers.length === 0
-        );
-        response.json({ questions: filteredQuestions });
-      } else {
-        response.json({ questions });
+  News.find(query)
+    .populate("requests")
+    .then((news) => {
+        res.json({ news });
       }
-    })
-    .catch((err) => response.send({ Error: err }));
+    )
+    .catch((err) => res.send({ Error: err }));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,52 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-
+const { response } = require('express');
 const app = express();
-const port = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3000; 
 
+let FT_API = require('./FT_API.json');
 
 app.use(express.json());
 app.use(bodyParser.json());
-
-const uri = process.env.ATLAS_URI;
-
-app.get('/', function (req, res) {
-  res.send('Hello World hhhh')
-})
+//app.use(express.static("FrontEnd"));
 
 
-app.listen(port, () => {
-  console.log(`Running at \`http://localhost:${port}\`...`);
+// show all the news 
+app.get('/', function(req, res){
+  console.log("first step");
+  console.log(FT_API)
+  const query = {};
+  const news = FT_API
+  res.json(FT_API)
+  .then((news) => {
+    res.json({ news });
+  })
+  .catch((err) => res.send({ Error: err }));
+});
+// end  
+
+// search the world 
+
+app.get('/search/:key', (req, res) => {
+  console.log("I got it ");
+  const search = req.body;
+  console.log(search)
+      FT_API.find(search).toArray(function(error, result) {
+        if(error){
+          response.status(500).send(error);
+            }else if (result) {
+              response.status(200).send(result);
+            } else {
+              response.sendStatus(404)
+            }
+  //const searchNews = FT_API.filter(newss => newss.name === search || newss.description === search)
+  
+  });
+});
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Running at \`http://localhost:${PORT}\`...`);
 });
