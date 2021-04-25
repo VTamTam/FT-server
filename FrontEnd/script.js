1131981b438708f055d90868974d833fd9670caa
Erwin Zarghani <erwinzarghani@Erwins-MacBook-Pro.local>
2021-04-25 19:17:01 +0100

    next
    next

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,38 +1,64 @@
+function fetchOne() {
+	fetch('/api')
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			if (!data) {
+				return null;
+			} else {
+				console.log(data);
+				data.forEach((element) => {
+					const news = document.createElement('div');
+					const date = document.createElement('h2');
+					const imageNew = document.createElement('img');
+					const news2 = document.createElement('div');
+					const title = document.createElement('h2');
+					const header = document.createElement('h2');
+					const description = document.createElement('p');
+
+					main.appendChild(news);
+					main.appendChild(date);
+					main.appendChild(imageNew);
+					main.appendChild(news2);
+					news2.appendChild(title);
+					news2.appendChild(header);
+					news2.appendChild(description);
+
+					main.classList.add('main');
+					news2.classList.add('news2');
+					imageNew.classList.add('image');
+					description.classList.add('description');
+					title.classList.add('title');
+
+					date.innerText = element.collections.id.createdAt;
+					imageNew.src = element.collections.id.url;
+					title.innerText = element.collections.id.name;
+					header.innerText = element.collections.id.id;
+					description.innerText = element.collections[0].description;
+				});
+			}
+		})
+		.catch((error) => console.log(error));
+}
+const searchNews = (event) => {
+	const searchInput = document.getElementById('search-input').value;
+	console.log('result: ', searchInput);
+	const lowCase = searchInput;
+	//console.log(searchInput , "this is the search")
+	fetch(`/search/{lowCase}`)
+		.then((res) => {
+			console.log(res);
+		})
+		.catch((err) => console.log(err));
+	event.preventDefault();
+};
 function setup() {
-    
-    const rootElem = document.getElementById("root");
-    const searchButton = document.getElementById("search-button");
-    const searchInput = document.getElementById("search-input").value;
-    
-    fetch('/')
-      .then((data) => {
-          res.json();
-          console.log(data)
-          console.log("fetch")
-    })
-    .catch((error) => console.log(error));
-
-    console.log("value " , searchInput);
-    searchButton.addEventListener("click", searchNews);
-};
-
-const searchNews = () => {
-    console.log("hhhhhhhhhhh" , )
-    const lowCase = "brexit";
-    //console.log(searchInput , "this is the search")
-    const option = {
-        method: 'GET',
-        Headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(lowCase)
-    };
-    fetch('/search', option)
-    .then(res => {
-        console.log(res);
-    })
-    .catch((err) => console.log(err));
-};
-
+	const rootElem = document.getElementById('root');
+	const searchButton = document.getElementById('search-button');
+	const main = document.getElementById('main');
+	searchButton.addEventListener('click', (event) => searchNews(event));
+	fetchOne();
+}
 
 window.onload = setup;
--- server.js
+++ server.js
@@ -8,11 +8,11 @@ let FT_API = require('./FT_API.json');
 
 app.use(express.json());
 app.use(bodyParser.json());
-//app.use(express.static("FrontEnd"));
+app.use(express.static("FrontEnd"));
 
 
 // show all the news 
-app.get('/', function(req, res){
+app.get('/api', function(req, res){
   console.log("first step");
   console.log(FT_API)
   const query = {};
@@ -26,26 +26,16 @@ app.get('/', function(req, res){
 // end  
 
 // search the world 
-
 app.get('/search/:key', (req, res) => {
   console.log("I got it ");
-  const search = req.body;
-  console.log(search)
-      FT_API.find(search).toArray(function(error, result) {
-        if(error){
-          response.status(500).send(error);
-            }else if (result) {
-              response.status(200).send(result);
-            } else {
-              response.sendStatus(404)
-            }
-  //const searchNews = FT_API.filter(newss => newss.name === search || newss.description === search)
-  
+  const {key} = req.params;
+  console.log(key)
+  const API = FT_API;
+  const final = API.collections.filter(
+    (el) => {el.name.indexOf === key.indexOf || el.description.indexOf === key.indexOf}
+  )
+  res.json({final})
   });
-});
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Running at \`http://localhost:${PORT}\`...`);
