27d201b0b7a3a66c70037dd2a824b0c6e06992e8
Erwin Zarghani <erwinzarghani@Erwins-MacBook-Pro.local>
2021-04-26 23:37:09 +0100

    1-1

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ let API_TV = require('./TV.json');
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.static('FrontEnd'));
+
 // show all the news
-app.get('/api', function(req, res) {
-	res.json(API_TV);
+app.get('/api', paginated(API_TV), (req, res) => {
+
+  res.json(res.paginatedResults);
 });
 // end
+
 // search the world
-app.get('/search/:key', (req, res) => {
+app.get('/search/:key', paginated(API_TV),(req, res) => {
 	const key = req.params.key;
 	console.log(key);
 	if (key.length > 0 ) {
@@ -29,6 +32,33 @@ app.get('/search/:key', (req, res) => {
 	}
 });
 // end
+
+function paginated(params) {
+  return (req, res, next) => {
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+
+    const results = {}
+    if (startIndex > 0 ) {
+      results.next = {
+        page: page + 1,
+        limit: limit
+      }
+    }
+    if (endIndex < params.length ) {
+      results.previous = {
+      page: page - 1,
+      limit: limit
+      }
+    }
+    results.results = params.slice(startIndex, endIndex)
+    res.paginatedResults = (results)
+    next()
+  }
+}
 app.listen(PORT, () => {
 	console.log(`Running at \`http://localhost:${PORT}\`...`);
-});
+}); 
