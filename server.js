23bb2a80f3adab39fa730e0a1e87828c967da109
Erwin Zarghani <erwinzarghani@Erwins-MacBook-Pro.local>
2021-04-27 17:31:58 +0100

    API is connect to the server

--- server.js
+++ server.js
@@ -6,15 +6,36 @@ let API_TV = require('./TV.json');
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.static('FrontEnd'));
-
+const fetch = require('node-fetch');
+var OriginalData;
 // show all the news
-app.get('/api', paginated(API_TV), (req, res) => {
-  res.json(res.paginatedResults);
+app.get('/api', fetchAPI(OriginalData),(req, res) => {
+  // res.json(res.OrginalD);
+  console.log("Original: " ,OriginalData)
+  //res.json(res.paginatedResults);
   console.log("result");
-  console.log(res.paginatedResults);
 });
 // end
-
+function fetchAPI(params) {
+  return (req, res, next) => {
+    const bodySend = {
+      "queryString": "banks",
+      "resultContext" : {
+        "aspects" :[  "title","lifecycle","location","summary","editorial" ]
+      }
+    };
+  
+  fetch(`https://api.ft.com/content/search/v1`,{
+      method: 'POST',
+      body: JSON.stringify(bodySend),
+      headers: { 'Content-Type': 'application/json' , 'X-Api-Key' : '59cbaf20e3e06d3565778e7b9758f7892e89468293a48663b98bd1d9' }
+  })
+    .then((res) => res.json())
+    .then(data => {params = data.results[0].results ; console.log("resuold orginal paroms is ok " , params)})
+    .catch((error) => console.log(error));
+    //res.OrginalD(params); 
+    next()
+  }}
 // search the world
 app.get('/search/:key', paginated(API_TV),(req, res) => {
 	const key = req.params.key;
@@ -42,7 +63,7 @@ function paginated(params) {
 
     const startIndex = (page - 1) * limit
     const endIndex = page * limit
-
+    // this part its add the next page and pervious page on result and I don't need it now.
     // const results = {}
     // if (startIndex > 0 ) {
     //   results.next = {
